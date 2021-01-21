3de5f7bf698c55b107a77b2107d09bdc886b6f49
Lincoln Miguel <lincolncampinas@hotmail.com>
2021-01-21 17:04:14 -0300

    Pasta com exercicio Aula 9 - ListaDeCompra

---
+++ lista-de-compras/src/App.js
@@ -0,0 +1,15 @@
+import Item from './Item'
+
+function App() {
+  return (
+    <div className="App">
+         <Item nome="Abacaxi" />
+         <Item nome="Uva" />
+         <Item nome="Laranja" />
+         <Item nome="Jaca" />
+         <Item nome="Banana" />
+    </div>
+  );
+}
+
+export default App;
